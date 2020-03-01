39e674c32a7e6dee3aeb8fed41a7b2ff51f15805
Fanilo RAVELOSON <faniloraveloson@MacBook-Pro-de-Fanilo.local>
2020-03-01 19:52:36 +0100

    added url connection to mongdb

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
-
+const url = "mongodb://heroku_x8kmrd20:rgco0s35rqt0u0fo9udrf143on@ds237267.mlab.com:37267/heroku_x8kmrd20" || "mongodb://127.0.0.1:27017/hygiene"
 mongoose
-    .connect('mongodb://127.0.0.1:27017/hygiene', { useNewUrlParser: true })
+    .connect(url, { useNewUrlParser: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
